2e55d763d36f8e19f2dffe02204c92c1499eb0ad
Matteo Piccini <gnappo.1@live.it>
2023-04-25 16:20:24 -0700

    done

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,8 @@
-from turtle import ht
+# from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')#, multi_valued_attributes=None)
+import ipdb; ipdb.set_trace()
